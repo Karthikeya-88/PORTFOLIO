80e90fc59ad742d51a27af4792e750eda5bc8216
Karthikeya <dkarthikeya888@gmail.com>
2025-03-29 12:30:08 +0530

    seventh

--- src/App.js
+++ src/App.js
@@ -162,123 +162,134 @@ function App() {
         />
         <hr style={{ width: "80%", border: "2px solid seashell" }} />
         <div
-          className="mb-2 p-4 text-black rounded border border-black"
+          className="mb-2 text-black"
           id="myResume"
-          style={{
-            backgroundColor: "rgba(246, 246, 200, 0.9)",
-            width: "94vw",
-            backdropFilter: "blur(8px)",
-            border: "1px solid rgba(255, 255, 255, 0.3)",
-          }}
+          style={{ width: "94vw" }}
         >
           <h2 className="text-center text-info">My Resume</h2>
-          <h6
-            className="m-4 resume-text text-center"
-            style={{ fontWeight: "600" }}
+          <div
+            style={{
+              backgroundColor: "rgba(246, 246, 200, 0.9)",
+              backdropFilter: "blur(8px)",
+              border: "1px solid rgba(255, 255, 255, 0.3)",
+            }}
+            className="rounded border border-black p-2"
           >
-            Download My Resume:{" "}
-            <a
-              href="https://drive.google.com/file/d/1iLjL1v946ZiBAqocyWq0DhOcco4hiwVQ/view?usp=sharing"
-              className="resume-el"
+            <h6
+              className="m-4 resume-text text-center"
+              style={{ fontWeight: "600" }}
             >
-              <IoNavigate
-                style={{
-                  marginLeft: "8px",
-                  filter: "drop-shadow(2px 2px 4px rgba(0,0,0,0.1))",
-                }}
-              />
-            </a>
-          </h6>
-          <div className="text-center pb-2">
-            <marquee
-              behavior="scroll"
-              direction="right"
-              scrollamount="8"
-              width="40%"
-            >
-              <h4 className="text-center text-warning open-to-relocation">
-                Open to Relocation
-              </h4>
-            </marquee>
+              Download My Resume:{" "}
+              <a
+                href="https://drive.google.com/file/d/1CdJGVegQ71k6Zx-EyZG3vRUaxVmlk3-x/view?usp=sharing"
+                className="resume-el"
+              >
+                <IoNavigate
+                  style={{
+                    marginLeft: "8px",
+                    filter: "drop-shadow(2px 2px 4px rgba(0,0,0,0.1))",
+                  }}
+                />
+              </a>
+            </h6>
+            <div className="text-center pb-2">
+              <marquee
+                behavior="scroll"
+                direction="right"
+                scrollamount="8"
+                width="40%"
+              >
+                <h4 className="text-center text-warning open-to-relocation">
+                  Open to Relocation
+                </h4>
+              </marquee>
+            </div>
+            <h4 className="mb-3 text-center">
+              I'm flexible regarding job types
+            </h4>
           </div>
-          <h4 className="mb-3 text-center">I'm flexible regarding job types</h4>
         </div>
         <hr style={{ width: "80%", border: "2px solid seashell" }} />
         <div
-          className="mb-2 p-4 text-black rounded border border-light"
-          style={{
-            backgroundColor: "#e1f1fa",
-            width: "96vw",
-            boxShadow: "0 8px 32px rgba(0,0,0,0.05)",
-            transform: "translateZ(0)",
-          }}
+          className="mb-2 text-black"
           id="myExperience"
+          style={{ width: "96vw" }}
         >
           <h2 className="text-center text-info">Experience</h2>
-          <div className="text-center pl-4 pr-4 p-3 w-100">
-            <h4 className="experience-header">
-              <b>Teaching Assistant - Nxtwave</b>
-            </h4>
-          </div>
-          <ul
+          <div
+            className="rounded border border-light p-2"
             style={{
-              listStyle: "disc",
+              backgroundColor: "#e1f1fa",
+              boxShadow: "0 8px 32px rgba(0,0,0,0.05)",
+              transform: "translateZ(0)",
             }}
-            className="text-black"
           >
-            <li
-              className="experience-li"
+            <div className="text-center pl-4 pr-4 p-3 w-100">
+              <h4 className="experience-header">
+                <b>Teaching Assistant - Nxtwave</b>
+              </h4>
+            </div>
+            <ul
               style={{
-                paddingBottom: "10px",
-                lineHeight: "1.6",
-                font: "20px Garamond, Perpetua",
+                listStyle: "disc",
               }}
+              className="text-black"
             >
-              As a Teaching Assistant for CCBPian learners, I provided hands-on
-              support in mastering the MERN stack, Programming with Python and
-              Querying with SQL. My role involved breaking down complex
-              concepts, debugging code, and equipping students with practical
-              strategies to tackle technical challenges independently.
-            </li>
-            <li
-              className="experience-li"
-              style={{
-                paddingBottom: "10px",
-                lineHeight: "1.6",
-                font: "20px Garamond, Perpetua",
-              }}
-            >
-              I focused on nurturing critical thinking by guiding learners to
-              diagnose issues and refine their coding approaches. Through
-              collaborative troubleshooting and clear communication, I helped
-              students build confidence in their ability to solve problems and
-              deliver functional solutions.
-            </li>
-          </ul>
-          <div className="text-justify">
-            <h4 style={{ color: "#4445ea", fontFamily: "Lora" }}>
-              <b>Skills Acquired:</b>
-            </h4>
-            <ol
-              style={{
-                font: "20px Garamond, Perpetua",
-              }}
-              className="text-dark"
-            >
-              {" "}
-              <li style={{ paddingBottom: "10px" }}>
-                Debugging & troubleshooting
+              <li
+                className="experience-li"
+                style={{
+                  paddingBottom: "10px",
+                  lineHeight: "1.6",
+                  font: "20px Garamond, Perpetua",
+                }}
+              >
+                As a Teaching Assistant for CCBPian learners, I provided
+                hands-on support in mastering the MERN stack, Programming with
+                Python and Querying with SQL. My role involved breaking down
+                complex concepts, debugging code, and equipping students with
+                practical strategies to tackle technical challenges
+                independently.
               </li>
-              <li style={{ paddingBottom: "10px" }}>
-                Addressed full-stack queries with precision
+              <li
+                className="experience-li"
+                style={{
+                  paddingBottom: "10px",
+                  lineHeight: "1.6",
+                  font: "20px Garamond, Perpetua",
+                }}
+              >
+                I focused on nurturing critical thinking by guiding learners to
+                diagnose issues and refine their coding approaches. Through
+                collaborative troubleshooting and clear communication, I helped
+                students build confidence in their ability to solve problems and
+                deliver functional solutions.
               </li>
-              <li style={{ paddingBottom: "10px" }}>
-                Problem-solving & analytical thinking
-              </li>{" "}
-              <li style={{ paddingBottom: "10px" }}>
-                Professional and a refined touch of formal response
-              </li>
-            </ol>
+            </ul>
+            <div className="text-justify">
+              <h4 style={{ color: "#4445ea", fontFamily: "Lora" }}>
+                <b>Skills Acquired:</b>
+              </h4>
+              <ol
+                style={{
+                  font: "20px Garamond, Perpetua",
+                }}
+                className="text-dark"
+              >
+                {" "}
+                <li style={{ paddingBottom: "10px" }}>
+                  Debugging & troubleshooting
+                </li>
+                <li style={{ paddingBottom: "10px" }}>
+                  Addressed full-stack queries with precision
+                </li>
+                <li style={{ paddingBottom: "10px" }}>
+                  Problem-solving & analytical thinking
+                </li>{" "}
+                <li style={{ paddingBottom: "10px" }}>
+                  Professional and a refined touch of formal response
+                </li>
+              </ol>
+            </div>
           </div>
         </div>
         <hr style={{ width: "80%", border: "2px solid seashell" }} />
@@ -288,77 +299,78 @@ function App() {
         </div>
         <hr style={{ width: "80%", border: "2px solid seashell" }} />
         <div
-          className="mb-2 p-4 text-dark rounded border border-dark section-hidden"
+          className="mb-2 section-hidden"
           id="myCert"
-          style={{
-            width: "94vw",
-            backgroundColor: "#e1f1fa",
-            backdropFilter: "blur(8px)",
-            border: "1px solid rgba(255, 255, 255, 0.3)",
-          }}
-          ref={(el) => (sectionRefs.current[4] = el)}
+          style={{ width: "94vw" }}
+          ref={(el) => (sectionRefs.current[1] = el)}
         >
-          <h2 className="text-center text-info">My Certifications</h2>
-          <div className="cert-container">
-            <img
-              src="https://res.cloudinary.com/dnmyyqfhs/image/upload/v1735840048/Screenshot_1099_m8bbkf.png"
-              alt="IRC"
-              className="cert-images"
-            />
-            <img
-              src="https://res.cloudinary.com/dnmyyqfhs/image/upload/v1735840467/Screenshot_1100_tb3izq.png"
-              alt="React"
-              className="cert-images"
-            />
-            <img
-              src="https://res.cloudinary.com/dnmyyqfhs/image/upload/v1735840499/Screenshot_1103_pktdia.png"
-              alt="Node"
-              className="cert-images"
-            />
-            <img
-              src="https://res.cloudinary.com/dnmyyqfhs/image/upload/v1735840532/Screenshot_1104_ocewaj.png"
-              alt="Javascript"
-              className="cert-images"
-            />
-            <img
-              src="https://res.cloudinary.com/dnmyyqfhs/image/upload/v1736395354/Screenshot_1113_zdqxtx.png"
-              alt="Dynamic web application"
-              className="cert-images"
-            />
-            <img
-              src="https://res.cloudinary.com/dnmyyqfhs/image/upload/v1736395265/Screenshot_1112_bv7wda.png"
-              alt="Developer foundations"
-              className="cert-images"
-            />
+          <h2 className="text-center">My Certifications</h2>
+          <div
+            className="p-4 rounded border border-dark text-dark"
+            style={{
+              backgroundColor: "#e1f1fa",
+              backdropFilter: "blur(8px)",
+              border: "1px solid rgba(255, 255, 255, 0.3)",
+            }}
+          >
+            <div className="cert-container">
+              <img
+                src="https://res.cloudinary.com/dnmyyqfhs/image/upload/v1735840048/Screenshot_1099_m8bbkf.png"
+                alt="IRC"
+                className="cert-images"
+              />
+              <img
+                src="https://res.cloudinary.com/dnmyyqfhs/image/upload/v1735840467/Screenshot_1100_tb3izq.png"
+                alt="React"
+                className="cert-images"
+              />
+              <img
+                src="https://res.cloudinary.com/dnmyyqfhs/image/upload/v1735840499/Screenshot_1103_pktdia.png"
+                alt="Node"
+                className="cert-images"
+              />
+              <img
+                src="https://res.cloudinary.com/dnmyyqfhs/image/upload/v1735840532/Screenshot_1104_ocewaj.png"
+                alt="Javascript"
+                className="cert-images"
+              />
+              <img
+                src="https://res.cloudinary.com/dnmyyqfhs/image/upload/v1736395354/Screenshot_1113_zdqxtx.png"
+                alt="Dynamic web application"
+                className="cert-images"
+              />
+              <img
+                src="https://res.cloudinary.com/dnmyyqfhs/image/upload/v1736395265/Screenshot_1112_bv7wda.png"
+                alt="Developer foundations"
+                className="cert-images"
+              />
 
-            <img
-              src="https://res.cloudinary.com/dnmyyqfhs/image/upload/v1735840562/Screenshot_1101_w9wwmb.png"
-              alt="Programming with Python"
-              className="cert-images"
-            />
-            <img
-              src="https://res.cloudinary.com/dnmyyqfhs/image/upload/v1735840597/Screenshot_1102_mwwqvt.png"
-              alt="Sql database"
-              className="cert-images"
-            />
-            <img
-              src="https://res.cloudinary.com/dnmyyqfhs/image/upload/v1739202289/Screenshot_1159_g9nela.png"
-              alt="Responsive web application"
-              className="cert-images"
-            />
-            <img
-              src="https://res.cloudinary.com/dnmyyqfhs/image/upload/v1739168961/Screenshot_1156_m4mwqc.png"
-              alt="Static web application"
-              className="cert-images"
-            />
+              <img
+                src="https://res.cloudinary.com/dnmyyqfhs/image/upload/v1735840562/Screenshot_1101_w9wwmb.png"
+                alt="Programming with Python"
+                className="cert-images"
+              />
+              <img
+                src="https://res.cloudinary.com/dnmyyqfhs/image/upload/v1735840597/Screenshot_1102_mwwqvt.png"
+                alt="Sql database"
+                className="cert-images"
+              />
+              <img
+                src="https://res.cloudinary.com/dnmyyqfhs/image/upload/v1739202289/Screenshot_1159_g9nela.png"
+                alt="Responsive web application"
+                className="cert-images"
+              />
+              <img
+                src="https://res.cloudinary.com/dnmyyqfhs/image/upload/v1739168961/Screenshot_1156_m4mwqc.png"
+                alt="Static web application"
+                className="cert-images"
+              />
+            </div>
           </div>
         </div>
         <hr style={{ width: "80%", border: "2px solid seashell" }} />
         <div className="mt-0 pt-0" id="askMe">
-          <AskMeSection
-            className="m-2 section-hidden"
-            ref={(el) => (sectionRefs.current[5] = el)}
-          />
+          <AskMeSection className="m-2" />
         </div>
         <hr style={{ width: "80%", border: "2px solid seashell" }} />
         <div
